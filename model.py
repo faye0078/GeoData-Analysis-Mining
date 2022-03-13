550f4b4f8b74a2aa185d775b6d99c201039f66ef
mick mo <faye892322@gmail.com>
2022-03-13 15:38:36 +0800

    the first work over

--- model.py
+++ model.py
@@ -1,15 +1,80 @@
+from tkinter.messagebox import NO
 import numpy as np
+import random
 from sklearn.cluster import KMeans
+from torch import nn
 # def PCA():
 
 class myKMeans():
-    def __init__(self, classes):
-        return 0
+    """
+    KMeans clustering
+
+    Parameters
+    ---
+    classes : int
+        the number of algo class
+    
+    Note:
+    ---
+    The k-means problem is solved using algorithm from Machine Learning written by Zhi-Hua Zhou.
+        
+    """
+    def __init__(self, classes, max_iter=1000):
+        self.classes = classes
+        self.max_iter = max_iter
+
+    def fit_predict(self, Y):
+        sample_idxes = random.sample(range(0, len(Y)), self.classes)
+        sample_idxes.sort()
+        avg_vec = [None] * len(Y)
+        update_vec = [Y[sample_idx] for sample_idx in sample_idxes]
+        
+        iter = 0
+        while iter < self.max_iter:
+            avg_vec = update_vec
+            predict_indexes = []
+
+            for j in range(len(Y)):
+                min_distance = float('inf')
+                min_idx = None
+
+                for class_idx in range(len(avg_vec)):
+                    distance = np.sqrt(np.sum(np.square(Y[class_idx] - Y[j])))
+                    if distance < min_distance:
+                        min_distance = distance
+                        min_idx = class_idx
+                predict_indexes.append(min_idx)
+
+            predict_num = [0] * len(Y)
+            for i, predict in enumerate(predict_indexes):
+                update_vec[predict] += Y[i]
+                predict_num[predict] += 1
+            for k in range(len(avg_vec)):
+                update_vec[k] /= predict_num[k]
+            
+            iter += 1
+
+        return predict_indexes
 
 def get_KMeans(type, classes):
     if type == 'sklearn':
         return KMeans(n_clusters=classes, random_state = 666)
     elif type == 'mine':
-        return myKMeans(classes)
+        return myKMeans(classes, max_iter=200)
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size=1, num_layers=2):
+        super(RNN, self).__init__()
+
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers) # rnn
+        self.reg = nn.Linear(hidden_size, output_size) # reg
+
+    def forward(self, x):
+        x, _ = self.rnn(x) # (seq, batch, hidden)
+        s, b, h = x.shape
+        x = x.view(s*b, h)
+        x = self.reg(x)
+        x = x.view(s, b, -1)
+        return x
 
     
--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import pandas as pd
 from collections import OrderedDict
 import numpy as np
 
-# 读文件，并按国家名称返回Dict类型数据
 def read_data(filename, used_countries):
     csv_covid_data = pd.read_csv(filename)
     country_covid_data = OrderedDict()
@@ -14,20 +13,40 @@ def read_data(filename, used_countries):
     
     return country_covid_data
 
-# 将每个国家的数据按一波疫情进行标准化
-def normalize(data):
-    # 去除nan值
+def preprocessing(data):
+
     country_data = data
     country_data = country_data[::-1]
     country_data = country_data[~np.isnan(country_data)]
 
-    # 获取一波疫情数据
     pre_min_idx = country_data[:country_data.argmax() + 1].argmin()
     clip_country_data = country_data[pre_min_idx : country_data.argmax()+1]
 
-    # 插值为长度为60的向量
     xp = np.linspace(1, len(clip_country_data), len(clip_country_data))
     xvals = np.linspace(1, len(clip_country_data), 60)
     normalized_data = np.interp(xvals, xp, clip_country_data)
 
     return normalized_data
+
+def create_dataloader(dataset, look_back=20):
+    
+    max_value = np.max(dataset)
+    min_value = np.min(dataset)
+    scalar = max_value - min_value
+    dataset = list(map(lambda x: x / scalar, dataset))
+
+    dataX, dataY = [], []
+    for i in range(len(dataset)):
+        for j in range(len(dataset[i]) - look_back):
+            a = dataset[i][j:(j + look_back)]
+            dataX.append(a)
+            dataY.append(dataset[i][j + look_back])
+    data_X = np.array(dataX)
+    data_Y = np.array(dataY)
+
+    train_size = int(len(data_X) * 0.8)
+    train_X = data_X[:train_size].reshape(-1, 1, look_back)
+    train_Y = data_Y[:train_size].reshape(-1, 1, 1)
+    test_X = data_X[train_size:].reshape(-1, 1, look_back)
+    test_Y = data_Y[train_size:].reshape(-1, 1, 1)
+    return {'input': train_X, 'predict': train_Y},  {'input':test_X, 'predict': test_Y}
